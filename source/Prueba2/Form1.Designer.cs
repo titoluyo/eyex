bf95895fc6eb80bfe89b80c228ca569643ffa4b0
Tito Luyo Murata <tluyo@hotmail.com>
2015-03-21 00:32:19 -0500

    prueba interseccion

--- source/Prueba2/Form1.Designer.cs
+++ source/Prueba2/Form1.Designer.cs
@@ -32,12 +32,15 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.behaviorMap1 = new EyeXFramework.Forms.BehaviorMap(this.components);
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Red;
-            this.panel1.Location = new System.Drawing.Point(27, 69);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 100);
             this.panel1.TabIndex = 0;
@@ -45,16 +48,43 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Blue;
-            this.panel2.Location = new System.Drawing.Point(363, 273);
+            this.panel2.Location = new System.Drawing.Point(231, 12);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(200, 100);
             this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Red;
+            this.panel3.Location = new System.Drawing.Point(450, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Red;
+            this.panel4.Location = new System.Drawing.Point(12, 132);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 100);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Red;
+            this.panel5.Location = new System.Drawing.Point(158, 189);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(200, 100);
+            this.panel5.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(626, 468);
+            this.ClientSize = new System.Drawing.Size(662, 468);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -68,6 +98,9 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private EyeXFramework.Forms.BehaviorMap behaviorMap1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
     }
 }
 
--- source/Prueba2/Form1.cs
+++ source/Prueba2/Form1.cs
@@ -20,7 +20,10 @@ namespace Prueba2
 
             Program.EyeXHost.Connect(behaviorMap1);
             behaviorMap1.Add(panel1, new GazeAwareBehavior(OnGaze));
-            behaviorMap1.Add(panel1, new GazeAwareBehavior(OnGaze));
+            behaviorMap1.Add(panel2, new GazeAwareBehavior(OnGaze));
+            behaviorMap1.Add(panel3, new GazeAwareBehavior(OnGaze));
+            behaviorMap1.Add(panel4, new GazeAwareBehavior(OnGaze));
+            behaviorMap1.Add(panel5, new GazeAwareBehavior(OnGaze));
         }
 
         private void OnGaze(object sender, GazeAwareEventArgs e)
