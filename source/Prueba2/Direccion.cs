310838e05200f4f045aa4e692bad4f8391bd976b
Tito Luyo Murata <tluyo@hotmail.com>
2015-03-21 01:02:14 -0500

    Direccion

---
+++ source/Prueba2/Direccion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba2
+{
+    public class Direccion : Panel
+    {
+        public int Horizontal { get; set; }
+        public int Vertical { get; set; }
+    }
+}
--- source/Prueba2/Form1.Designer.cs
+++ source/Prueba2/Form1.Designer.cs
@@ -29,59 +29,159 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new Prueba2.Direccion();
+            this.panel2 = new Prueba2.Direccion();
             this.behaviorMap1 = new EyeXFramework.Forms.BehaviorMap(this.components);
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel3 = new Prueba2.Direccion();
+            this.panel4 = new Prueba2.Direccion();
+            this.panel5 = new Prueba2.Direccion();
+            this.panel6 = new Prueba2.Direccion();
+            this.panel7 = new Prueba2.Direccion();
+            this.panel8 = new Prueba2.Direccion();
+            this.panel9 = new Prueba2.Direccion();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHorizontal = new System.Windows.Forms.TextBox();
+            this.txtVertical = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Horizontal = -1;
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 100);
             this.panel1.TabIndex = 0;
+            this.panel1.Vertical = 1;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Blue;
+            this.panel2.Horizontal = 0;
             this.panel2.Location = new System.Drawing.Point(231, 12);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(200, 100);
             this.panel2.TabIndex = 1;
+            this.panel2.Vertical = 1;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.Red;
+            this.panel3.Horizontal = 1;
             this.panel3.Location = new System.Drawing.Point(450, 12);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(200, 100);
             this.panel3.TabIndex = 1;
+            this.panel3.Vertical = 1;
             // 
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.Red;
+            this.panel4.Horizontal = -1;
             this.panel4.Location = new System.Drawing.Point(12, 132);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(200, 100);
             this.panel4.TabIndex = 2;
+            this.panel4.Vertical = 0;
             // 
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.Red;
-            this.panel5.Location = new System.Drawing.Point(158, 189);
+            this.panel5.Horizontal = 0;
+            this.panel5.Location = new System.Drawing.Point(231, 132);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(200, 100);
             this.panel5.TabIndex = 3;
+            this.panel5.Vertical = 0;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Red;
+            this.panel6.Horizontal = 1;
+            this.panel6.Location = new System.Drawing.Point(450, 132);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(200, 100);
+            this.panel6.TabIndex = 4;
+            this.panel6.Vertical = 0;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Red;
+            this.panel7.Horizontal = -1;
+            this.panel7.Location = new System.Drawing.Point(12, 258);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(200, 100);
+            this.panel7.TabIndex = 5;
+            this.panel7.Vertical = -1;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Red;
+            this.panel8.Horizontal = 0;
+            this.panel8.Location = new System.Drawing.Point(231, 258);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(200, 100);
+            this.panel8.TabIndex = 5;
+            this.panel8.Vertical = -1;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Red;
+            this.panel9.Horizontal = 1;
+            this.panel9.Location = new System.Drawing.Point(450, 258);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(200, 100);
+            this.panel9.TabIndex = 5;
+            this.panel9.Vertical = -1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 380);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Horizontal:";
+            // 
+            // txtHorizontal
+            // 
+            this.txtHorizontal.Location = new System.Drawing.Point(77, 377);
+            this.txtHorizontal.Name = "txtHorizontal";
+            this.txtHorizontal.ReadOnly = true;
+            this.txtHorizontal.Size = new System.Drawing.Size(100, 20);
+            this.txtHorizontal.TabIndex = 7;
+            // 
+            // txtVertical
+            // 
+            this.txtVertical.Location = new System.Drawing.Point(299, 377);
+            this.txtVertical.Name = "txtVertical";
+            this.txtVertical.ReadOnly = true;
+            this.txtVertical.Size = new System.Drawing.Size(100, 20);
+            this.txtVertical.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Vertical:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 468);
+            this.ClientSize = new System.Drawing.Size(662, 405);
+            this.Controls.Add(this.txtVertical);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHorizontal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
@@ -90,17 +190,26 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private Direccion panel1;
+        private Direccion panel2;
         private EyeXFramework.Forms.BehaviorMap behaviorMap1;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel5;
+        private Direccion panel3;
+        private Direccion panel4;
+        private Direccion panel5;
+        private Direccion panel6;
+        private Direccion panel7;
+        private Direccion panel8;
+        private Direccion panel9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHorizontal;
+        private System.Windows.Forms.TextBox txtVertical;
+        private System.Windows.Forms.Label label2;
     }
 }
 
